741f1d37b34fd161db52c65ba3d126c37cad2372
Moudy <moudy.elkammash@gmail.com>
2013-07-02 12:15:34 -0400

    hero focus and collapse

--- _source/_assets/app.js
+++ _source/_assets/app.js
@@ -1,3 +1,3 @@
 if (!App.isTouch()) {
-  App.headerView = new App.HeaderView({ el: '#header' });
+  App.headerView = new App.HeaderView({ el: '#site-header' });
 }
--- _source/_assets/header_view.js
+++ _source/_assets/header_view.js
@@ -1,18 +1,29 @@
 window.App.HeaderView = Backbone.View.extend({
   initialize: function () {
+    this.$aboutMe = this.$('.about-me');
+    this.$footer = this.$('.footer');
     this.$img = this.$('.avatar-image');
     this.setDimensions();
 
     this.state = new Backbone.Model();
     this.listenTo(this.state, 'change:scale', this.onScaleChange);
+    if (App.isPost) this.listenTo(this.state, 'change:heroFocus', this.onHeroFocusChange);
 
     this.$window = $(window);
     var onScroll = _.throttle(this.onScroll.bind(this), 10);
     this.$window.on('scroll', onScroll);
   }
 
+, events: {
+    'click h1, h2': 'onHClick'
+  }
+
+, onHClick: function () {
+    $('html, body').animate({ scrollTop: 0 }, 80);
+  }
+
 , setDimensions: function () {
-    var imageMarginTop = 20;
+    var imageMarginTop = 10;
 
     this.imageOffset = this.$img.offset();
     this.imageHeight = this.$img.outerHeight();
@@ -42,10 +53,37 @@ window.App.HeaderView = Backbone.View.extend({
     // round up to two decimal places
     scale = Math.ceil(scale * 100) / 100;
 
-    this.state.set({ scale: scale });
+    this.state.set({
+      scale: scale
+    , heroFocus: (scale >= 1)
+    });
+  }
+
+, onHeroFocusChange: function (state, heroFocus) {
+    $('body').toggleClass('hero-focus', heroFocus);
   }
 
 , onScaleChange: function (state, scale) {
+    var aboutMeCss = {}
+      , footerCss = {}
+      , ABOUT_ME_HEIGHT = 140
+      , FOOTER_HEIGHT = 40;
+
+    aboutMeCss.height = ABOUT_ME_HEIGHT * scale;
+    aboutMeCss.opacity = scale;
+    aboutMeCss['-webkit-transform'] = 'scaleY(' + scale + ')';
+    //aboutMeCss['-webkit-transform'] = 'scaleX(' + scale + ')';
+
+    footerCss.height = FOOTER_HEIGHT * scale;
+    footerCss.opacity = scale;
+    footerCss['-webkit-transform'] = 'scaleY(' + scale + ')';
+
+    console.log(footerCss, this.$footer)
+
+    this.$footer.css(footerCss);
+    this.$aboutMe.css(aboutMeCss);
+    console.log('scale', scale)
+
     this.$img.css({
       '-webkit-transform': 'scale(' + scale + ')'
     });
