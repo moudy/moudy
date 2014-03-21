d38ac58af7c890b4a8a09aa3083b6d75509447d0
Moudy <moudy.elkammash@gmail.com>
2014-03-21 19:29:35 -0400

    rm old assests

--- assets/app-4848b81101478aec90512c1419ef6b08.js
+++
@@ -1,4 +0,0 @@
-(function () {
-  var de = document.documentElement;
-  de.className = de.className+' ready';
-})();
--- assets/app-6b4d1a6e24ec768d6b56b2caacc138e1.js
+++
@@ -1,6 +0,0 @@
-(function () {
-  setTimeout(function () {
-  var de = document.documentElement;
-  de.className = de.className+' ready';
-  },10);
-})();
