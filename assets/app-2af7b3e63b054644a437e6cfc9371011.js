4776d73decfee0dc525a5f06fa1631715d428de7
Moudy <moudy.elkammash@gmail.com>
2014-05-20 08:50:05 -0400

    add post

---
+++ assets/app-2af7b3e63b054644a437e6cfc9371011.js
@@ -0,0 +1,27 @@
+
+(function () {
+
+  function addClass (el, className) {
+    el.className = el.className+' '+className;
+  }
+
+  var documentEl = document.documentElement;
+
+  setTimeout(function () {
+    addClass(documentEl, 'ready');
+  },10);
+
+
+  var links = document.links;
+
+  for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+    if (links[i].hostname != window.location.hostname) {
+      links[i].target = '_blank';
+    }
+  }
+
+  if (window.location.pathname !== '/') {
+    addClass(documentEl, 'not-home');
+  }
+
+})();
