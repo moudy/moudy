814cb7efce53659980d425f6c08b19ee47dd1d3a
Moudy <moudy.elkammash@gmail.com>
2014-07-29 18:53:29 -0400

    cleanup

---
+++ site/pages/data.js
@@ -0,0 +1,17 @@
+
+exports.about = [
+  { text: 'Available for select projects' }
+, { text: 'Based in Flatiron, New York City' }
+];
+
+exports.experience = [
+  { name: 'Codecademy', url: 'http://www.codecademy.com/' }
+, { name: 'BestVendor', url: 'http://bestvendor.com/' }
+];
+
+exports.projects = [
+  { name: 'WorkOf', url: 'http://www.workof.com/' }
+, { name: 'ViceHd', url: 'https://github.com/vrsity/vicehd' }
+, { name: 'NYPD', url: 'http://www.n-y-p-d.com/' }
+, { name: 'Photography', url: 'http://photo.moudy.me/' }
+];
